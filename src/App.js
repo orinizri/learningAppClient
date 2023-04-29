ceaddd92a8bf088cf5cbf1f33ee3d7dccbe8f107
Ori Nizri Tal <Ori@YABIDEV002.yali.local>
2023-04-29 12:23:34 +0300

    29.04

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Nav from "./nav";
 
 function App() {
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState([]);
 
     useEffect(() => {
-        console.log("use effect");
-        fetch("http://localhost:8080/message")
-            .then((res) => res.json())
-            .then((data) => setMessage(data.message));
-    }, [message]);
+        (async function getUser() {
+            console.log("use effect");
+            let users = await axios.get("http://localhost:8080/users");
+            console.log("getUser - users:", users.data);
+            setMessage(users.data);
+        })();
+    }, []);
 
     return (
         <div className="App">
+            <Nav />
             <header className="App-header">
                 new react app
-                <div>{message}</div>
+                <div>
+                    {message.map((user) => {
+                        console.log("user");
+                        return (
+                            <>
+                                <ul key={user.id}>
+                                    <li>user: {user.username}</li>
+                                    <li>password: {user.password}</li>
+                                    <li>age: {user.age}</li>
+                                    <li>email: {user.email}</li>
+                                </ul>
+                                <div></div>
+                            </>
+                        );
+                    })}
+                </div>
             </header>
         </div>
     );
---
+++ src/nav.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import React from "react";
+
+function Nav() {
+    console.log("nav")
+    return (<div>Nav bar</div>);
+}
+
+export default Nav;
